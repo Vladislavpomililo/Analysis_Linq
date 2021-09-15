1907b814807d4fc2030d9307f9dbe087ebb269f1
Vladislavpomililo <Vladislav.pomililo@yandex.ru>
2021-09-16 02:49:12 +0300

    Добавьте файлы проекта.

---
+++ Analysis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Analysis_Linq
+{
+    class Analysis
+    {
+        public static void AnalysisText(object adressObj )
+        {
+            //Таймер времени обработки текста
+            Stopwatch stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
+
+            string adressFile = adressObj.ToString();
+
+            string text = File.ReadAllText(adressFile, Encoding.GetEncoding(65001));
+            //запросом LINQ и методом Parts сортируем обьекты 
+            var groups = Parts(text, 3)
+               .Where(str => str.All(ch => char.IsLetter(ch)))
+               .GroupBy(str => str);
+            // запросом LINQ фильтруем обьекты
+            Console.WriteLine(string.Join(Environment.NewLine, groups.OrderByDescending(gr => gr.Count()).Take(10).Select(gr => $"\"{gr.Key}\" встретилось {gr.Count()} раз")));
+
+            //останавливаем таймер и переводим полученый результат в миллисекунды.
+            stopWatch1.Stop();
+            TimeSpan ts1 = stopWatch1.Elapsed;
+            var time = ts1.Minutes * 60000 + ts1.Seconds * 1000 + ts1.Milliseconds;
+            Console.WriteLine($"\nВремя работы программы: " + time + " миллисекунд.");
+        }
+
+        private static IEnumerable<string> Parts(string source, int length)
+        {
+            for (int i = length; i <= source.Length; i++)
+                yield return source.Substring(i - length, length);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Analysis_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к текстовому файлу:");
+            string adressFile = Console.ReadLine();
+
+            //Передаем во второй поток параметр и запускаем его.
+            Thread threadAnalisisText = new Thread(new ParameterizedThreadStart(Analysis.AnalysisText));
+            threadAnalisisText.Start(adressFile);
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
